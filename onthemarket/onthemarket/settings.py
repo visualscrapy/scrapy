fdb2a1c3c406784df2d66bb9543f7c4f2a9ede4d
visualscrapy <visualscrapy@gmail.com>
2020-12-19 23:55:09 +0530

    removed onthemarket

--- onthemarket/onthemarket/settings.py
+++
@@ -1,8 +0,0 @@
-BOT_NAME = 'onthemarket'
-
-SPIDER_MODULES = ['onthemarket.spiders']
-NEWSPIDER_MODULE = 'onthemarket.spiders'
-
-ROBOTSTXT_OBEY = False
-
-USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36"
